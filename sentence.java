b77ac62975bf30a5c3f5b43226d9e62a60f1588a
Kalyani-Gali <120916892+Kalyani-Gali@users.noreply.github.com>
2023-02-20 22:35:39 +0530

    updated sentence.java

--- sentence.java
+++ sentence.java
@@ -1,28 +1,22 @@
-import java.util.Scanner;
-public class methods{
-    public static void main(String[]args){
-System.out.print("Enter a 6-digit number: ");
-int number = input.nextInt();
-
-int oddSum = 0;
-int evenSum = 0;
-
-while (number > 0) {
-   int digit = number % 10;
-
-   if (digit % 2 == 0) {
-      evenSum += Math.pow(digit, 2);
-   } else {
-      oddSum += Math.pow(digit, 3);
-   }
-
-   number /= 10;
+import java.util.*;
+public class sentence{
+public static void WordFrequencies(String str) {
+   HashMap<String, Integer> c = new HashMap<>();
+    String[] s = str.split("");
+    for (String w : s) {
+        if (c.containsKey(w)) {
+            c.put(w, c.get(w) + 1);
+        } else {
+            c.put(w, 1);
+        }
+    }
+    for (String w : c.keySet()) {
+        c.put(w,c.get(w));
+    }
+    System.out.println(c);
 }
-
-if (evenSum != 0) {
-   int division = oddSum / evenSum;
-   System.out.println("The division of the sum of the cubes of odd digits and sum of the squares of even digits is: " + division);
-} else {
-   System.out.println("The sum of even digits is zero, cannot divide.");
+public static void main(String[]args)
+{String str="hii hello hiii how are you you";
+   WordFrequencies(str);
 }
-}}
+}
